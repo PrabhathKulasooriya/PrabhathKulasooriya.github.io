6a89382054a397bcfbe444c2f86ac5855e55a0f7
Prabhath Kulasooriya <prabhath.kulasooriya@gmail.com>
2026-01-06 22:21:14 +0530

    Bug fixes

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -22,30 +22,32 @@ export const projects = [
     description:
       "An AI-powered chatbot featuring natural language processing and image generation. Includes real-time chat functionality, secure session management, and Stripe payment integration.",
     type: "Featured Project",
-    live_link: "",
+    live_link: "https://quickgpt-sigma.vercel.app/",
     github_repo: "https://github.com/PrabhathKulasooriya/quickGPT-AI-chatbot",
     images: [image_3],
   },
   {
-    id: 2,
+    id: 3,
     title: "E-Commerce Website",
     tech: ["React", "Node.js", "Express", "MongoDB", "JWT"],
     description:
       "A comprehensive e-commerce platform with product management, shopping cart functionality, and a secure admin dashboard for order and user management.",
     type: "Web Application",
-    live_link:"",
-    github_repo: "https://github.com/PrabhathKulasooriya/Fabrilia-clothing-store",
+    live_link: "",
+    github_repo:
+      "https://github.com/PrabhathKulasooriya/Fabrilia-clothing-store",
     images: [image_1, image_2],
   },
   {
-    id: 3,
+    id: 4,
     title: "Online Movie Ticket Booking System",
     tech: ["Laravel", "MySQL", "Blade Templates", "Bootstrap"],
     description:
       "Complete cinema booking system allowing seat selection, show schedules, payment integration, and ticket management. Includes a robust admin panel for analytics and manage movies and showtimes.",
     type: "Full Stack System",
     live_link: "",
-    github_repo: "https://github.com/PrabhathKulasooriya/Cineverse-Movie-ticket-booking-website",
+    github_repo:
+      "https://github.com/PrabhathKulasooriya/Cineverse-Movie-ticket-booking-website",
     images: [image_4, image_5, image_6],
   },
 ];
